54bacfa4929369855da651e5f98833de52367292
EduardoMorales <edalmoes98@gmail.com>
2022-04-26 15:29:14 -0500

    Agregando prueba de unidad requerimiento 2

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,9 +1,15 @@
 const ExplorerService=require("./../../lib/services/ExplorerService.js")
 
 describe("Test para ExplorerService",()=>{
-    test("Requerimiento 1: Calcular todos los explorers",()=>{
-        const explorers=[{mission:"node"},{mission:"node"},{mission:"node"}]
+    test("Requerimiento 1: Obtener lista de los explorers en determinada misión",()=>{
+        const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}]
         const explorersInNode=ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(3)
+        expect(explorersInNode.length).toBe(2)
+    })
+
+    test("Requerimiento 2: Calcular el número de explorers en determinada misión",()=>{
+        const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}]
+        const numberExplorersInNode=ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        expect(numberExplorersInNode).toBe(2)
     })
 })
