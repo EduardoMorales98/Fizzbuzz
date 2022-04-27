eff5568ef8ca9978c490ec2f542cf53ac00e3493
EduardoMorales <edalmoes98@gmail.com>
2022-04-27 13:45:09 -0500

    Prueba de unidad ExplorerController para obtener numero de explorers en node

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,16 +1,20 @@
 const ExplorerController=require("./../../lib/controllers/ExplorerController.js");
 describe("Test para ExplorerController",()=>{
     test("1. Obtener lista de los explorers que están en node",()=>{
-        const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}];
         const explorersInNode=ExplorerController.getExplorersByMission("node");
         //console.log(explorersInNode)
         expect(explorersInNode.length).toBe(10);
     });
 
     test("2. Obtener la lista de usuarios de github de los explorers que están en node",()=>{
-        const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}];
         const UsersExplorersInNode=ExplorerController.getExplorersUsernamesByMission("node");
         //console.log(UsersExplorersInNode)
         expect(UsersExplorersInNode.length).toBe(10);
+    });
+
+    test("3. Calcular el número de explorers que están en node",()=>{
+            const numberExplorersInNode=ExplorerController.getExplorersAmonutByMission("node");
+            //console.log(numberExplorersInNode)
+            expect(numberExplorersInNode).toBe(10);
     });
 });
