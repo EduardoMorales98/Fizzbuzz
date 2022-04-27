23d26d41dc01404ddce44d0852ac6eb5ec24025e
EduardoMorales <edalmoes98@gmail.com>
2022-04-26 20:46:30 -0500

    Agregando prueba de unidad requerimiento 3

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,6 +4,7 @@ describe("Test para ExplorerService",()=>{
     test("Requerimiento 1: Obtener lista de los explorers en determinada misión",()=>{
         const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}]
         const explorersInNode=ExplorerService.filterByMission(explorers, "node")
+        console.log(explorersInNode)
         expect(explorersInNode.length).toBe(2)
     })
 
@@ -11,5 +12,12 @@ describe("Test para ExplorerService",()=>{
         const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}]
         const numberExplorersInNode=ExplorerService.getAmountOfExplorersByMission(explorers, "node")
         expect(numberExplorersInNode).toBe(2)
+    })
+
+    test("Requerimiento 3: Obtener la lista de usuarios de github de los explorers que están en node",()=>{
+        const explorers=[{githubUsername: "ajolonauta1",mission:"node"},{githubUsername: "ajolonauta2",mission:"node"},{githubUsername: "ajolonauta3",mission:"java"}]
+        const UsersExplorersInNode=ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        console.log(UsersExplorersInNode)
+        expect(UsersExplorersInNode.length).toBe(2)
     })
 })
