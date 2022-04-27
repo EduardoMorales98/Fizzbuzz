8dbe4966c9e8b60e2eb670b0cffc1043e5716e6f
EduardoMorales <edalmoes98@gmail.com>
2022-04-27 13:39:49 -0500

    Prueba de unidad ExplorerController para obtener lista de usuarios de github de explorers en node

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,10 +1,16 @@
 const ExplorerController=require("./../../lib/controllers/ExplorerController.js");
 describe("Test para ExplorerController",()=>{
-    test("Obtener lista de los explorers que están en node",()=>{
+    test("1. Obtener lista de los explorers que están en node",()=>{
         const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}];
         const explorersInNode=ExplorerController.getExplorersByMission("node");
         //console.log(explorersInNode)
         expect(explorersInNode.length).toBe(10);
     });
 
+    test("2. Obtener la lista de usuarios de github de los explorers que están en node",()=>{
+        const explorers=[{mission:"node"},{mission:"node"},{mission:"java"}];
+        const UsersExplorersInNode=ExplorerController.getExplorersUsernamesByMission("node");
+        //console.log(UsersExplorersInNode)
+        expect(UsersExplorersInNode.length).toBe(10);
+    });
 });
